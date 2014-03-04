e0b1ed59a66cd1e0ad555148a7485dd6a3b446cc
Andrea Warren <melaa24@gmail.com>
2014-03-04 15:06:29 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Food App!"
       redirect_to @user
     else
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :studio,
-      							   :ayurveda, :password,
+      							               :ayurveda, :password,
                                    :password_confirmation)
     end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,14 @@
 include ApplicationHelper
 
+def valid_signup(page)
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Studio",		    with: "Yoga Now"
+  fill_in "Ayurveda",	    with: "Dr. Feel Good"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
 def full_title(page_title)
   base_title = "Food App"
   if page_title.empty?
